4b1927096eef4544b5114b48ab28cc66f0bc43f5
Zach <zachriane01@gmail.com>
2022-09-12 13:44:14 +0800

    [Exercise 1] Fixed files and other misc

---
+++ Exercise 1/calculateGrades/src/calculateGrades.java
@@ -0,0 +1,24 @@
+/* ======================================================
+FILENAME    :calculateGrades.java
+DESCRIPTION :Create a program that asks for 5 grades and
+            displays their average.
+AUTHOR      :Zach Riane I. Machacon
+CREATED     :September 10, 2022
+======================================================= */
+
+import java.util.Scanner;
+
+public class calculateGrades {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double grade, sum = 0, average;
+        int i;
+        for(i = 0; i < 5; i++){
+            System.out.print("Enter Grade #" + (i+1) + ": ");
+            grade = input.nextDouble();
+            sum += grade;
+        }
+        average = sum / 5.0;
+        System.out.println("Your average is: " + average);
+    }
+}
